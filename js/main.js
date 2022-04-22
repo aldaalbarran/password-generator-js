b88e4aa38389422f3bf93cee7acb6a055cd045af
aldaalbarran <aldaxalbarran@gmail.com>
2022-04-22 01:39:53 -0500

    Eventos: 1. No refresh, 2. Button Minus, 3. Button plus

--- js/main.js
+++ js/main.js
@@ -16,4 +16,25 @@
 		capitalLetters: 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z',
 		lowercaseLetters: 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
 	}
+
+	/* ---------------------------------------- Events ------------------------------ */
+	//Evitar refresh en la página
+	app.addEventListener('submit', function(e){
+		e.preventDefault();
+	});
+
+	//Aumentar 1 cada que se presiona el botón +
+	app.elements.namedItem('btn-plus').addEventListener('click', function(){
+		config.characters++;
+		inputCharacters.value = config.characters;
+	});
+
+	//Disminuir 1 cada que se presiona el botón -
+	app.elements.namedItem('btn-minus').addEventListener('click', function(){
+		if (config.characters > 1) {
+			config.characters--;
+			inputCharacters.value = config.characters;
+		}
+	});
+
 }())
